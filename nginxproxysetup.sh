c2ebcf1c9c765ec0c80c01c3bcceec16f57e5012
Jaco van Zyl <jvanzyl5@outlook.com>
2023-07-06 09:32:08 +0200

    Initial Commit

---
+++ nginxproxysetup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Function to check if a directory exists
+is_directory_available() {
+    [ -d "$1" ]
+}
+
+# Check if certbot and python3-certbot-nginx are already installed
+if dpkg -s "certbot" >/dev/null 2>&1 && dpkg -s "python3-certbot-nginx" >/dev/null 2>&1; then
+    echo "certbot and python3-certbot-nginx are already installed."
+else
+    # Install certbot and python3-certbot-nginx
+    sudo apt install -y certbot python3-certbot-nginx
+fi
+
+# Prompt the user for input
+read -p "Enter the proxy address (e.g., example.com): " proxy_address
+read -p "Enter the target address (e.g., http://localhost:8000): " target_address
+
+# Determine the configuration file names and paths
+sites_available_dir="/etc/nginx/sites-available"
+sites_enabled_dir="/etc/nginx/sites-enabled"
+base_config_file="reverse_proxy"
+config_file="$sites_available_dir/$base_config_file.conf"
+counter=2
+
+# Check if the sites-available directory exists
+if ! is_directory_available "$sites_available_dir"; then
+    echo "Directory $sites_available_dir does not exist. Please make sure NGINX is installed correctly."
+    exit 1
+fi
+
+# Check if the sites-enabled directory exists
+if ! is_directory_available "$sites_enabled_dir"; then
+    echo "Directory $sites_enabled_dir does not exist. Please make sure NGINX is installed correctly."
+    exit 1
+fi
+
+# Check if the base configuration file already exists
+while [ -e "$config_file" ]; do
+    config_file="$sites_available_dir/${base_config_file}_$counter.conf"
+    counter=$((counter + 1))
+done
+
+# Generate the NGINX configuration file
+if cat > "$config_file" << EOF
+server {
+    listen 80;
+    server_name $proxy_address;
+
+    location / {
+        proxy_pass $target_address;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOF
+then
+    echo "NGINX configuration file has been generated at $config_file."
+
+    # Create a symbolic link in sites-enabled
+    if ln -s "$config_file" "$sites_enabled_dir/$(basename "$config_file")"
+    then
+        echo "A symbolic link has been created in $sites_enabled_dir."
+
+        # Restart NGINX to apply the configuration
+        if sudo service nginx restart >/dev/null 2>&1
+        then
+            echo "NGINX has been restarted to apply the configuration."
+        else
+            echo "Failed to restart NGINX. Please make sure NGINX is installed correctly."
+            exit 1
+        fi
+    else
+        echo "Failed to create symbolic link. Please check the permissions of $sites_enabled_dir."
+        exit 1
+    fi
+else
+    echo "Failed to generate NGINX configuration file. Please check the permissions of $sites_available_dir."
+    exit 1
+fi
